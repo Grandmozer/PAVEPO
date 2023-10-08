9611d474bd18e4e7aa8623e0b5c9f459cce2ec77
Grandmozer <svetoskover@gmail.com>
2023-10-08 21:44:19 +0500

    bacground made in %

--- js/swiper.js
+++ js/swiper.js
@@ -1,12 +1,29 @@
 
 const swiper = new Swiper('.swiper', {
 // Optional parameters
-slidesPerView: 3,   
-spaceBetween:30,
-direction: 'horizontal',
+// slidesPerView: 3,   
+// spaceBetween:30,
+// direction: 'horizontal',
+// loop: true,
+// spaceBetween: 50,        
+
+slidesPerView: 5,
+centeredSlides: true,
+spaceBetween: 30,
 loop: true,
-
-
+loopedSlides: 7,
+watchSlidesVisibility: true,
+breakpoints: {
+ 
+    1920: {
+        slidesPerView:3,
+        // spaceBetween: 30,
+    },
+    480: {
+        slidesPerView:1,
+        // spaceBetween: 10,
+    }
+},
 // If we need pagination
 pagination: {
     el: '.swiper-pagination',
