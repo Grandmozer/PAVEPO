d76c0b320db68f1e70ee23d7a3d4f015c398dcc4
Grandmozer <svetoskover@gmail.com>
2023-10-17 21:39:32 +0500

    new swiper

--- js/swiper.js
+++ js/swiper.js
@@ -1,66 +1,44 @@
 const swiperSmall = document.getElementById('swiper-mini');
 
+
 const swiper = new Swiper(swiperSmall, {
-// Optional parameters
-slidesPerView: 3,   
-spaceBetween:-390,
-direction: 'horizontal',
-loop: true,
-// spaceBetween: 30,        
-centeredSlides: true,
-
-// loopedSlides: 7,
-// watchSlidesVisibility: true,
-breakpoints: {
- 
-    480: {
-        slidesPerView:1,
-        // spaceBetween: 10,
+    // Optional parameters
+    direction: 'horizontal',
+    slidesPerView: 5,
+    centeredSlides: true,
+    spaceBetween: 30,
+    loop: true,
+    loopedSlides: 7,
+    watchSlidesVisibility: true,
+    // If we need pagination
+    pagination: {
+      el: '.swiper-pagination',
     },
-    900:{
-        slidesPerView:2,
-
+  
+    // Navigation arrows
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
     },
-    1200:{
-        slidesPerView:2,
-    },
-    1600:{
-        slidesPerView:2,
-        spaceBetween: -700,
-    },
-    1700:{
-        slidesPerView:3,
-        spaceBetween: -290,
-    },
-    1800:{
-        slidesPerView:3,
-        spaceBetween: -240,
-    },
-    1900: {
-        slidesPerView:3,
-        spaceBetween: -210,
-    },
-    1920:{
-        slidesPerView:3,
-        spaceBetween: -250,
-    }
-},
-
-
-// If we need pagination
-pagination: {
-    el: '.swiper-pagination',
-},
-
-// Navigation arrows
-navigation: {
-    nextEl: '.swiper-button-next',
-    prevEl: '.swiper-button-prev',
-},
-
-// And if we need scrollbar
-scrollbar: {
-    el: '.swiper-scrollbar',
-},
-});
+      breakpoints: {
+  
+          1028: {
+              slidesPerView:4,
+              spaceBetween: 30,
+          },
+          768: {
+            slidesPerView:3,
+            spaceBetween: 20,
+        },
+          480: {
+              slidesPerView:1,
+              spaceBetween: 10,
+          },
+          300:{
+            slidesPerView:1,
+            spaceBetween: 10,
+          }
+      }
+  });
+  
 
