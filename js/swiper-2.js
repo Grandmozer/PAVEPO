9604637d38ab4e5c30d54850fb0e4e86aaf88587
Grandmozer <svetoskover@gmail.com>
2023-10-19 21:28:30 +0500

    two page have swiper

--- js/swiper-2.js
+++ js/swiper-2.js
@@ -1,42 +1,54 @@
 const swiperMidle = document.getElementById('swiper-midle');
 
 const swiper = new Swiper(swiperMidle, {
-// Optional parameters
-slidesPerView: 3,   
-spaceBetween: 40,
-direction: 'horizontal',
-loop: true,
-// spaceBetween: 30,        
-centeredSlides: true,
-
-// loopedSlides: 7,
-// watchSlidesVisibility: true,
-breakpoints: {
- 
-    1920: {
-        slidesPerView:3,
-        // spaceBetween: 30,
+    // Optional parameters
+    direction: 'horizontal',
+    slidesPerView: 5,
+    centeredSlides: true,
+    spaceBetween: 100,
+    loop: true,
+    loopedSlides: 7,
+    watchSlidesVisibility: true,
+    // If we need pagination
+    pagination: {
+      el: '.swiper-pagination',
     },
-    480: {
-        slidesPerView:1,
-        // spaceBetween: 10,
-    }
-},
-
-
-// If we need pagination
-pagination: {
-    el: '.swiper-pagination',
-},
-
-// Navigation arrows
-navigation: {
-    nextEl: '.swiper-button-next',
-    prevEl: '.swiper-button-prev',
-},
-
-// And if we need scrollbar
-scrollbar: {
-    el: '.swiper-scrollbar',
-},
-});
+  
+    // Navigation arrows
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+      breakpoints: {
+  
+          1700: {
+            slidesPerView:3,
+            spaceBetween: 100,
+          },
+          1200: {
+            slidesPerView:3,
+            spaceBetween: 60,
+          },
+          1120:{
+            slidesPerView:3,
+            spaceBetween: 40,
+          },
+          1000:{
+            slidesPerView:3,
+            spaceBetween: 20,
+          },
+          800: {
+              slidesPerView:2,
+              spaceBetween: 30,
+          },
+          500:{
+            slidesPerView:1,
+            spaceBetween: 60,
+          },
+          300:{
+            slidesPerView:1,
+            spaceBetween: 60,
+          }
+      }
+  });
+  
