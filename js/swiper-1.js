4253528725d90f3b9931c9015168e8cd5929a894
Grandmozer <svetoskover@gmail.com>
2023-10-20 22:05:19 +0500

    swiper done

--- js/swiper-1.js
+++ js/swiper-1.js
@@ -1,7 +1,7 @@
 const swiperSmall = document.getElementById('swiper-mini');
+const swiperMidle = document.getElementById('swiper-midle');
 
-
-const swiper = new Swiper(swiperSmall, {
+const swiper1 = new Swiper(swiperSmall, {
     // Optional parameters
     direction: 'horizontal',
     slidesPerView: 5,
@@ -17,8 +17,9 @@ const swiper = new Swiper(swiperSmall, {
   
     // Navigation arrows
     navigation: {
-      nextEl: '.swiper-button-next',
-      prevEl: '.swiper-button-prev',
+      nextEl: '.slider1-next', 
+      prevEl: '.slider1-prev',
+      uniqueNavElements: false //разрешаем конфликты 
     },
       breakpoints: {
   
@@ -42,3 +43,60 @@ const swiper = new Swiper(swiperSmall, {
   });
   
 
+  const swiper2 = new Swiper(swiperMidle, {
+    // Optional parameters
+    direction: 'horizontal',
+    slidesPerView: 5,
+    centeredSlides: true,
+    spaceBetween: 100,
+    loop: true,
+    loopedSlides: 7,
+    watchSlidesVisibility: true,
+    // If we need pagination
+    pagination: {
+      el: '.swiper-pagination',
+    },
+  
+    // Navigation arrows
+    navigation: {
+      nextEl: '.slider2-next', 
+      prevEl: '.slider2-prev', 
+      uniqueNavElements: false //разрешаем конфликты 
+    },
+      breakpoints: {
+  
+          1700: {
+            slidesPerView:3,
+            spaceBetween: 100,
+          },
+          1200: {
+            slidesPerView:3,
+            spaceBetween: 60,
+          },
+          1120:{
+            slidesPerView:3,
+            spaceBetween: 40,
+          },
+          1000:{
+            slidesPerView:3,
+            spaceBetween: 20,
+          },
+          800: {
+              slidesPerView:2,
+              spaceBetween: 30,
+          },
+          500:{
+            slidesPerView:1,
+            spaceBetween: 60,
+          },
+          300:{
+            slidesPerView:1,
+            spaceBetween: 10,
+          },
+          250:{
+            slidesPerView:1,
+            spaceBetween: 60,
+          }
+      }
+  });
+  
